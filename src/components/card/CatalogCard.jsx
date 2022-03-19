fbc06dd1f13ca4cabf5cb93c372ed632aa282c72
Juan Carlos <jcarlosriego@gmail.com>
2022-03-19 21:39:13 +0100

    Added Material UI to Catalog and Catalog Card

--- src/components/card/CatalogCard.jsx
+++ src/components/card/CatalogCard.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React from "react"
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+  Box,
+  Grid,
+} from "@mui/material"
 
 const CatalogCard = ({ phoneData }) => {
-
-    return (
-        <h1>{phoneData.name}</h1>
-    )
+  //console.log(phoneData)
+  return (
+    <Grid item textAlign='center'>
+      <Card sx={{ width: 340, height: 550, padding: 2, marginTop: 2 }}>
+        <CardActionArea>
+          <Box
+            component="img"
+            src={`${process.env.REACT_APP_SERVER_DEV}${phoneData.imageFileName}`}
+            alt={`${phoneData.manufacturer} ${phoneData.name}`}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              color="primary"
+            >
+              {`${phoneData.manufacturer} ${phoneData.name}`}
+            </Typography>
+            <Typography variant="h4" component="div">
+              {`${phoneData.price} €`}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  )
 }
 
 export default CatalogCard
