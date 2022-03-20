0f4535d11d799ef317399d5513dcfa765d1ba463
Juan Carlos <jcarlosriego@gmail.com>
2022-03-20 16:10:30 +0100

    Added dialog to edit or add phones

--- src/components/card/CatalogCard.jsx
+++ src/components/card/CatalogCard.jsx
@@ -1,23 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import {
   Card,
   CardContent,
   Typography,
-  CardActionArea,
   Box,
   Grid,
+  Collapse,
+  CardActions,
+  IconButton,
 } from "@mui/material"
+import EditIcon from "@mui/icons-material/Edit"
+import DeleteIcon from "@mui/icons-material/Delete"
+
+import PhoneDialog from "../dialog/PhoneDialog"
 
 const CatalogCard = ({ phoneData }) => {
-  //console.log(phoneData)
+  const { name, manufacturer, price, imageFileName, description, screen, processor, ram } = phoneData
+
+  const [expanded, setExpanded] = useState(false)
+  const [openDialog, setOpenDialog] = useState(false)
+
+  const handleExpandClick = () => setExpanded(!expanded)
+  const handleDeleteClick = () => console.log("delete!")
+  const handleOpenDialog = () => setOpenDialog(true)
+  const handleCloseDialog = () => setOpenDialog(false)
+
   return (
-    <Grid item textAlign='center'>
-      <Card sx={{ width: 340, height: 550, padding: 2, marginTop: 2 }}>
-        <CardActionArea>
+    <>
+      <Grid item textAlign="center">
+        <Card
+          sx={{ width: 340, padding: 2, marginTop: 2, cursor:'pointer' }}
+          onClick={handleExpandClick}
+        >
           <Box
             component="img"
-            src={`${process.env.REACT_APP_SERVER_DEV}${phoneData.imageFileName}`}
-            alt={`${phoneData.manufacturer} ${phoneData.name}`}
+            src={`${process.env.REACT_APP_SERVER_DEV}${imageFileName}`}
+            alt={`${manufacturer} ${name}`}
           />
           <CardContent>
             <Typography
@@ -26,16 +44,47 @@ const CatalogCard = ({ phoneData }) => {
               component="div"
               color="primary"
             >
-              {`${phoneData.manufacturer} ${phoneData.name}`}
+              {`${manufacturer} ${name}`}
             </Typography>
             <Typography variant="h4" component="div">
-              {`${phoneData.price} €`}
+              {`${price} €`}
             </Typography>
           </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
-  )
-}
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary" textAlign="initial">
+                {description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" textAlign="initial">
+                <b>Screen: </b>{screen}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" textAlign="initial">
+                <b>Processor: </b>{processor}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" textAlign="initial">
+                <b>RAM Memory: </b>{ram} GB
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <IconButton aria-label="edit phone" onClick={handleOpenDialog}>
+                <EditIcon color="primary"/>
+              </IconButton>
+              <IconButton aria-label="delete phone" onClick={handleDeleteClick}>
+                <DeleteIcon color="error"/>
+              </IconButton>
+            </CardActions>
+          </Collapse>
+        </Card>
+      </Grid>
+      {openDialog && (
+        <PhoneDialog
+          open={openDialog}
+          handleClose={handleCloseDialog}
+          phone={phoneData}
+        />
+      )}
+    </>
+  );
+};
 
-export default CatalogCard
+export default CatalogCard;
--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,28 +1,49 @@
-import { Grid } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { getAllPhones } from '../../services/phones'
+import { Backdrop, Button, CircularProgress, Grid } from "@mui/material"
+import React, { useEffect, useState } from "react"
+import { getAllPhones } from "../../services/phones"
 
-import CatalogCard from '../card/CatalogCard'
+import CatalogCard from "../card/CatalogCard"
+import PhoneDialog from "../dialog/PhoneDialog"
 
 const Catalog = () => {
-    const [phones, setPhones] = useState([])
+  const [phones, setPhones] = useState([])
+  const [openDialog, setOpenDialog] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        let mounted = true
-        getAllPhones()
-            .then(items => {
-                if(mounted) {
-                    setPhones(items.data)
-                }
-            })
-        return () => mounted = false
-    }, [])
+  const handleClick = () => setOpenDialog(true)
+  const handleCloseDialog = () => setOpenDialog(false)
 
-    return (
-        <Grid container component="ul" spacing={3} justifyContent='center'>
-            {phones.map(phone => <CatalogCard key={phone._id} phoneData={phone} />)}
-        </Grid>
-    )
-}
+  useEffect(() => {
+    let mounted = true
+    setIsLoading(true)
+    getAllPhones().then((items) => {
+      if (mounted) {
+        setPhones(items.data)
+        setIsLoading(false)
+      }
+    });
+    return () => (mounted = false)
+  }, [])
 
-export default Catalog
+  return (
+    <>
+      <Button onClick={handleClick}>Add new Phone</Button>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Grid container component="ul" spacing={3} justifyContent="center">
+        {phones.map((phone) => (
+          <CatalogCard key={phone._id} phoneData={phone} />
+        ))}
+      </Grid>
+      {openDialog && (
+        <PhoneDialog open={openDialog} handleClose={handleCloseDialog} />
+      )}
+    </>
+  );
+};
+
+export default Catalog;
